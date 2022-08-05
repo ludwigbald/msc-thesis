03faa99160cb2e752ef4556de0a1ec34b16469fa
Ludwig Bald <hallo@ludwigbald.com>
2022-08-05 13:08:19 +0200

    further optimized ludwig_rbc parameters

--- agents/ludwig_rbc_agent.py
+++ agents/ludwig_rbc_agent.py
@@ -6,32 +6,16 @@ def rbc_policy(observation, action_space):
     """
     hour = observation[2] # Hour index is 2 for all observations
     solar = observation[21] # solar generation in kWh
+    load = observation[20] # non-shiftable load
 
     action = 0.0
     if 16 <= hour <= 20:
         # Afternoon (high prices): release/sell all stored energy
-        action = - 0.2
+        action = 0.2
     elif (1 <= hour <= 15) or (21 <= hour <= 24):
-        # Rest of Day: store all solar power,
-        action = min(1, solar/6.4) # where 6.4 kWh is battery capacity
-        # action should be the share of battery size (which is given in kW)
-
-    # if (8 <= hour <= 15):
-    #     # Around Noon: store all solar power when prices are low
-    #     action = min(1, solar/6.4) # where 6.4 kWh is battery capacity
-    #     # action should be the share of battery size (which is given in kW)
-    # elif 16 <= hour <= 20:
-    #     # Afternoon (high prices): release/sell as much stored energy as possible
-    #     action = -1 
-    # elif (21 <= hour <= 24) or (1 <= hour <= 2):
-    #     # prices are low and electicity is carbon intensive: do nothing
-    #     action = 0
-    # elif (2 <= hour <= 4):
-    #     # prices and carbon are low: buy lots of electricity
-    #     action = 1
-    # elif (5 <= hour <= 7):
-    #     # prices are still low, but carbon is higher: sell lots of electricity
-    #     action = -1
+        # Rest of Day: store enough solar power to get the battery near full
+        action = 0.6*solar/6.4 # where 6.4 kWh is battery capacity
+        # action should be the share of battery size (which is given in kWh)
     
 
 
